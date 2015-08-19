759fee8b7310396eec6a2b9514f3a1d34748274a
Lasse Nielsen <leni12@student.aau.dk>
2015-08-19 19:11:09 +0200

    dummy data, new database, new test db and ignore file

---
+++ createDatabase.sql
@@ -0,0 +1,6 @@
+CREATE TABLE sensor_data( id           integer primary key autoincrement not null
+                        , timestamp    datetime default current_timestamp not null
+                        , sensor_id    integer not null
+                        , value        real not null);
+CREATE TABLE sensors( sensor_id        integer primary key
+                    , sensor_name      text not null);
