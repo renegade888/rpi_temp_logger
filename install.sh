291b4624c12a3e6cb43c8c1f0f14710effef42c4
Lasse Nielsen <leni12@student.aau.dk>
2015-08-22 09:40:08 +0200

    reading

--- install.sh
+++ install.sh
@@ -20,3 +20,4 @@ git clone https://github.com/poohzrn/rpi_temp_logger
 sudo ln -s ~/rpi_temp_logger/monitor.py /usr/lib/cgi-bin/
 sudo chown www-data:www-data /usr/lib/cgi-bin/monitor.py
 sudo ln -s ~/rpi_temp_logger/webgui.py /var/www/tmplog/index.py
+#/boot/config.txt -> dtoverlay=w1-gpio
