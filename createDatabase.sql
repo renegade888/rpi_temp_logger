8a57ee94817065dc2d4399a86c5ed2dbfeb9f895
pi <leni12@student.aau.dk>
2015-08-22 08:45:08 +0000

    Dropper sensor tabellen for nu

--- createDatabase.sql
+++ createDatabase.sql
@@ -2,5 +2,7 @@ CREATE TABLE sensor_data( id           integer primary key autoincrement not nul
                         , timestamp    datetime default current_timestamp not null
                         , sensor_id    integer not null
                         , value        real not null);
+/*
 CREATE TABLE sensors( sensor_id        integer primary key
                     , sensor_name      text not null);
+*/
